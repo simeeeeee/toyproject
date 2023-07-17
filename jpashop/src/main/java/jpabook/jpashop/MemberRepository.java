11f5e48941d7a88ca651a46752cfdd832bdaa2b8
simeee <whdk2340@gmail.com>
2023-07-17 21:09:25 +0900

    first commit

---
+++ jpashop/src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,11 @@
+package jpabook.jpashop;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+}
